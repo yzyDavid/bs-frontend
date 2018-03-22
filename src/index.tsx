766082fbe8180ab2077d560b6d79563ed0e1ca29
Zhenyun Yu <yzyDavid@qq.com>
2018-03-22 15:00:51 +0800

    modifying to start.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
+import dva, {connect} from 'dva';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+const app = dva();
+
 registerServiceWorker();
