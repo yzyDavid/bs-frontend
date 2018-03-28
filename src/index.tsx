e6e2afeac2414c00ee330d034a3c3c11458edb49
Zhenyun Yu <yzyDavid@qq.com>
2018-03-29 02:05:58 +0800

    mothing

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import dva, {connect} from 'dva';
+import {browserHistory} from 'dva/router';
 import * as React from 'react';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const app = dva();
+const app = dva({
+    history: browserHistory
+});
+
+app.start('#root');
 
 registerServiceWorker();
---
+++ src/models/login.tsx
@@ -0,0 +1,10 @@
+export interface LoginState {
+    token: string;
+}
+
+const LoginModel = {
+    namespace: 'login',
+    reducers: {}
+};
+
+export default LoginModel;
