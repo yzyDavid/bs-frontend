7c99d5c6a5b2b45b963d1603e22aedd606288983
Zhenyun Yu <yzyDavid@qq.com>
2018-05-28 23:20:47 +0800

    template codes for manage model.

--- src/models/ManagementModel.ts
+++ src/models/ManagementModel.ts
@@ -1,8 +1,23 @@
+import { authFetch, checkAuthedOrLogout } from '../utils/auth';
+import { message } from 'antd';
+
 const ManagementModel = {
     namespace: 'management',
     state: {
         myWords: [],
         wordbooks: []
+    },
+    effects: {
+        * getWordbooks(payload: null | undefined, { call }) {
+            const response = yield call(authFetch, '/wordbook/all', 'GET');
+            // TODO:
+        }
+    },
+    reducers: {
+        setWordbooks(state) {
+            // TODO:
+            return null;
+        }
     }
 };
 
